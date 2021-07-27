c2d96b8404d8ac46e39c63956d768e90c9dfe317
邓志宇 <shoper2@163.com>
2021-07-27 09:11:45 +0800

    修改org-roam-capture模板

--- init.el
+++ init.el
@@ -146,12 +146,13 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam
+       (org
             +pomodoro
             +hugo
             +dragndrop
             +noter
             +pandoc
+            +roam
             )               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more

7caf683cc4db7937a8f5272592a257828f747bb7
dzy <shoper2@163.com>
2022-01-11 14:35:44 +0800

    修改主题

--- custom.el
+++ custom.el
@@ -3,10 +3,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2D2A2E" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
  '(custom-safe-themes
-   '("76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
+   '("4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
  '(doc-view-ghostscript-options
-   '("-dSAFER" "-dNOPAUSE" "-dTextAlphaBits=4" "-dBATCH" "-dGraphicsAlphaBits=4" "-r100" "-dQUIET")))
+   '("-dSAFER" "-dNOPAUSE" "-dTextAlphaBits=4" "-dBATCH" "-dGraphicsAlphaBits=4" "-r100" "-dQUIET"))
+ '(exwm-floating-border-color "#413f42")
+ '(fci-rule-color "#4C4A4D")
+ '(highlight-tail-colors ((("#393b35") . 0) (("#343b40") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#19181A" "#FCFCFA"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#19181A" "#A9DC76"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#19181A" "#727072"))
+ '(objed-cursor-color "#CC6666")
+ '(pdf-view-midnight-colors (cons "#FCFCFA" "#2D2A2E"))
+ '(rustic-ansi-faces
+   ["#2D2A2E" "#CC6666" "#A9DC76" "#FFD866" "#78DCE8" "#FF6188" "#78DCE8" "#FCFCFA"])
+ '(vc-annotate-background "#2D2A2E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A9DC76")
+    (cons 40 "#c5da70")
+    (cons 60 "#e2d96b")
+    (cons 80 "#FFD866")
+    (cons 100 "#fec266")
+    (cons 120 "#fdad66")
+    (cons 140 "#FC9867")
+    (cons 160 "#fd8572")
+    (cons 180 "#fe737d")
+    (cons 200 "#FF6188")
+    (cons 220 "#ee627c")
+    (cons 240 "#dd6471")
+    (cons 260 "#CC6666")
+    (cons 280 "#b56869")
+    (cons 300 "#9f6b6c")
+    (cons 320 "#886d6f")
+    (cons 340 "#4C4A4D")
+    (cons 360 "#4C4A4D")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

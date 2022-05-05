aacc7069222a2aeb8e70f486029dfcc15c606d58
dzy <shoper2@163.com>
2022-05-05 17:11:54 +0800

    fix org-roam-ui double open

--- config.el
+++ config.el
@@ -549,7 +549,7 @@
   (setq org-roam-ui-sync-theme t
         org-roam-ui-follow t
         org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
+        org-roam-ui-open-on-start nil))
 
 ;;视频笔记
 (use-package! org-media-note
